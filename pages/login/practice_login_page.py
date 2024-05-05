ecbbae333748f31f564b44223003dd8427563c06
Zaid García <angieillumi@gmail.com>
2024-05-04 20:01:26 -0600

    logout ok

--- pages/login/practice_login_page.py
+++ pages/login/practice_login_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 import time
 
@@ -10,7 +11,7 @@ class LoginView:
         self.wait = WebDriverWait(self.driver, 50)
 
     def fill_username_input(self, username, username_input):
-        time.sleep(2)
+        time.sleep(1)
         username_input = self.wait.until(EC.visibility_of_element_located((By.XPATH, username_input)))
         username_input.clear()
         username_input.send_keys(username)
@@ -24,14 +25,36 @@ class LoginView:
     def click_login_button(self, login_button_xpath):
         login_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, login_button_xpath)))
         login_button.click()
+    def click_logout_button(self, logout_button_xpath):
+        time.sleep(1)
+        logout_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, logout_button_xpath)))
+        logout_button.click()
         
-    def verify_login_successful(self, congrats_message_xpath):
+    def verify_login_successful(self):
         try:
-            self.wait.until(EC.visibility_of_element_located((By.XPATH, congrats_message_xpath)))
+            time.sleep(1)
+            
+            self.wait.until(EC.visibility_of_element_located((By.XPATH, "//h1[@class='post-title'][contains(.,'Logged In Successfully')]")))
             return True
-        except:
-            return False
-    
+        except NoSuchElementException as e:
+            print(f"Elemento no encontrado: {e.msg}")
+        except TimeoutException as e:
+            print(f"Tiempo de espera excedido para encontrar el elemento: {e.msg}")
+        except Exception as e:
+            print(f"Error inesperado: {str(e)}")
+        return False
+
+    def verify_logout_sucessful(self):
+        try:
+            time.sleep(1)
+            self.wait.until(EC.visibility_of_element_located((By.XPATH, "//h2[contains(.,'Test login')]")))
+            return True
+        except NoSuchElementException as e:
+            print(f"Elemento no encontrado: {e.msg}")
+        except TimeoutException as e:
+            print(f"Tiempo de espera excedido para encontrar el elemento: {e.msg}")
+        except Exception as e:
+            print(f"Error inesperado: {str(e)}")
 
     def login_successfully(self, username_input, username, password_input, password, login_button_xpath):
         self.driver.get('https://practicetestautomation.com/practice-test-login/')
@@ -39,6 +62,17 @@ class LoginView:
         self.fill_username_input(username, username_input)
         self.fill_password_input(password, password_input)
         self.click_login_button(login_button_xpath)
+        self.verify_login_successful()
+        
+    def logout_successfully(self,  username_input, username, password_input, password, login_button_xpath,logout_button_xpath):
+        self.driver.get('https://practicetestautomation.com/practice-test-login/')
+        self.driver.maximize_window()
+        self.fill_username_input(username, username_input)
+        self.fill_password_input(password, password_input)
+        self.click_login_button(login_button_xpath)
+        self.verify_login_successful()
+        self.click_logout_button(logout_button_xpath)
+        self.verify_logout_sucessful()
         
         
 
--- tests/login_module/test_login_page.py
+++ tests/login_module/test_login_page.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from pages.login.practice_login_page import LoginView
-from tests.config import  username_input,username,password_input,password,login_button_xpath,congrats_message_xpath
+from tests.config import  username_input, username, password_input, password, login_button_xpath,logout_button_xpath
 
 '''
 @pytest.fixture(scope="session")
@@ -20,6 +20,31 @@ def browser():
 '''
 
 def test_login_success(browser):
-    login_page = LoginView(browser)
-    login_page.login_successfully(username_input, username, password_input, password, login_button_xpath)
-    assert login_page.verify_login_successful(congrats_message_xpath) == True
+    login_page_view = LoginView(browser)
+    login_page_view.login_successfully(username_input,username, password_input,password, login_button_xpath)
+    verification_results = []
+    verification_results.append(login_page_view.verify_login_successful())
+    for i, result in enumerate(verification_results, start=1):
+        if result:
+            print(f"Verificación {i}: Éxito")
+        else:
+            print(f"Verificación {i}: Error")
+    if False in verification_results:
+        print("La prueba falló: Uno o más elementos no pudieron ser verificados correctamente.")
+    else:
+        print("La prueba fue exitosa: Todos los elementos fueron verificados correctamente.")
+        
+def test_logout_success(browser):
+    login_page_view = LoginView(browser)
+    login_page_view.logout_successfully(username_input,username, password_input,password, login_button_xpath,logout_button_xpath)
+    verification_results = []
+    verification_results.append(login_page_view.verify_logout_sucessful())
+    for i, result in enumerate(verification_results, start=1):
+        if result:
+            print(f"Verificación {i}: Éxito")
+        else:
+            print(f"Verificación {i}: Error")
+    if False in verification_results:
+        print("La prueba falló: Uno o más elementos no pudieron ser verificados correctamente.")
+    else:
+        print("La prueba fue exitosa: Todos los elementos fueron verificados correctamente.")
